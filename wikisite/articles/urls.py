ace49c47471144def2e83d219258b58409d1605b
Marcus <mstojcevich@gmail.com>
2018-10-09 15:56:58 -0400

    Create simple django project & app

---
+++ wikisite/articles/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+# This file contains routes to different views
+# within the "pages" app
+
+urlpatterns = [
+        path('', view=views.index, name='articles-index')
+]
