cd271bdd1f331f29dd0eeae30f221cd01e214a79
Anthony Powell <apowell@arbor.net>
2018-10-11 16:58:07 -0400

    Remove failing test so CI completes successfully

--- wikisite_frontend/src/App.test.js
+++ wikisite_frontend/src/App.test.js
@@ -7,7 +7,3 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
-
-it ('fails on purpose', () => {
-  throw new Error('I Crashed!')
-})
