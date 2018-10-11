2b8e94f3d999f5953b2aea7753e21d337d52479a
Anthony Powell <apowell@arbor.net>
2018-10-11 16:55:10 -0400

    Create failing test to ensure CI fails

--- wikisite_frontend/src/App.test.js
+++ wikisite_frontend/src/App.test.js
@@ -7,3 +7,7 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it ('fails on purpose', () => {
+  throw new Error('I Crashed!')
+})
