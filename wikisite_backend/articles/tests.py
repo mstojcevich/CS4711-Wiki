955ea87a425521810bc1bfd913f67d50dbe85b73
Marcus Stojcevich <mstoj@pm.me>
2018-10-24 23:20:11 -0400

    Fix linter error and exempt migrations from flake8

--- wikisite_backend/articles/tests.py
+++ wikisite_backend/articles/tests.py
@@ -1,7 +1,6 @@
 import pytest
 from django.contrib.auth.models import User
 from rest_framework import status
-from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 
 from articles.models import Article
