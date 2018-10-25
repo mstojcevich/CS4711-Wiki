c827fa356a0eeee3b6a5bf002aa9c7e04357a5a2
Marcus <mstojcevich@gmail.com>
2018-10-25 17:08:10 -0400

    Add ArticleRevision model

--- wikisite_backend/articles/models.py
+++ wikisite_backend/articles/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -5,6 +6,18 @@ class Article(models.Model):
     """
     A wiki article
     """
+
     creation_date = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=64, unique=True)
+    content = models.TextField()
+
+
+class ArticleRevision(models.Model):
+    """
+    A revision of a wiki article
+    """
+
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    creation_date = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
