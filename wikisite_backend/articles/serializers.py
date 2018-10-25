a446fd96050f1a4cea00a82af95055346f31e36b
Marcus <mstojcevich@gmail.com>
2018-10-25 17:47:42 -0400

    Add last updated datetime to article serializer

--- wikisite_backend/articles/serializers.py
+++ wikisite_backend/articles/serializers.py
@@ -8,10 +8,13 @@ class ArticleSerializer(serializers.HyperlinkedModelSerializer):
     """
 
     content = serializers.CharField(source="latest_revision.content", read_only=True)
+    last_updated = serializers.DateTimeField(
+        source="latest_revision.creation_date", read_only=True
+    )
 
     class Meta:
         model = Article
-        fields = ("url", "id", "creation_date", "name", "content")
+        fields = ("url", "id", "creation_date", "name", "content", "last_updated")
 
         extra_kwargs = {"creation_date": {"read_only": True}}
 
