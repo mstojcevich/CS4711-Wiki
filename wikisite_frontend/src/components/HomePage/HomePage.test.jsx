3f4a2957c9a9b2924895c4f4557210104a07d097
Anthony Powell <apowell@arbor.net>
2018-11-07 12:14:20 -0500

    Fix test partially

--- wikisite_frontend/src/components/HomePage/HomePage.test.jsx
+++ wikisite_frontend/src/components/HomePage/HomePage.test.jsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import HomePage from './HomePage';
 
+const props = {
+  isLoggedIn: () => false,
+  requests: {
+    getArticles: () => new Promise(resolve => resolve()),
+  },
+};
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Router><HomePage /></Router>, div);
+  ReactDOM.render(<Router><HomePage {...props} /></Router>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
