1af55a058ea6db07b3e5a7c1bfe35ddca4294897
manunandan95 <44533291+manunandan95@users.noreply.github.com>
2018-11-08 14:46:18 -0500

    Update UserSettings.test.js

--- wikisite_frontend/src/components/UserSettings/UserSettings.test.js
+++ wikisite_frontend/src/components/UserSettings/UserSettings.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import NotFound from './UserSettings';
+import UserSettings from './UserSettings';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
