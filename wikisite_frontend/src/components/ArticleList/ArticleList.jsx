273176367a51d97a76a0bf7ad3281afcfbc7e8e5
manunandan95 <44533291+manunandan95@users.noreply.github.com>
2018-11-08 15:18:24 -0500

    Create ArticleList.jsx

---
+++ wikisite_frontend/src/components/ArticleList/ArticleList.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Header } from 'semantic-ui-react';
+
+const ArticleList = () => (
+  <Header as="h1">404: Page not found!</Header>
+);
+
+export default ArticleList;
