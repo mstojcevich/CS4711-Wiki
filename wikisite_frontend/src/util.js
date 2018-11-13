d36c8222bbbae8dcdd3d97cf01167dabd8871350
Marcus Stojcevich <mstoj@pm.me>
2018-11-12 23:21:29 -0500

    Actual formatDate impl

--- wikisite_frontend/src/util.js
+++ wikisite_frontend/src/util.js
@@ -3,9 +3,8 @@
  * @param {String} dateString Date string as given by the API
  */
 function formatDate(dateString) {
-  // Just a no-op for now
-  // TODO implement
-  return dateString;
+  const date = new Date(dateString);
+  return date.toLocaleString();
 }
 
 export { formatDate };
