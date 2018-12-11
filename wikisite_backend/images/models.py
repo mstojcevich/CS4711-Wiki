3a9feae1e8894348afa31fc119a66fe721184c5f
Marcus Stojcevich <mstojcevich@users.noreply.github.com>
2018-12-11 14:30:01 -0500

    Make image comments optional (#81)
    * Make image comments optional
    
    * Add the models file too

--- wikisite_backend/images/models.py
+++ wikisite_backend/images/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Image(models.Model):
-    comments = models.TextField()  # Comments included with the image upload
+    comments = models.TextField(null=True)  # Comments included with the image upload
     data = models.ImageField(upload_to="image_uploads/")
     upload_date = models.DateTimeField(auto_now_add=True)
     uploaded_by = models.ForeignKey(User, on_delete=models.PROTECT)
