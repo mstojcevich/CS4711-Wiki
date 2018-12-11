f559f2bdec7c7eaeba07573abca4c01f03ec627f
Marcus Stojcevich <mstojcevich@users.noreply.github.com>
2018-12-11 12:05:24 -0500

    Locking backend (#78)
    * Add locked field to article
    
    * Add locked field to serializer, check locked field permissions
    
    * Add test for locked article
    
    * Add test for locked articles & superuser

--- wikisite_backend/articles/views.py
+++ wikisite_backend/articles/views.py
@@ -5,6 +5,20 @@ from articles.serializers import ArticleSerializer, ArticleListSerializer
 from articles.serializers import ArticleRevisionSerializer
 
 
+class LockedOrAdminPermission(permissions.BasePermission):
+    """
+    Permission that checks to see if the article is locked
+    or if the user is an admin
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if obj.locked and not request.user.is_superuser:
+            return False
+        else:
+            # Article is unlocked or the user is an admin
+            return True
+
+
 class ArticleViewSet(viewsets.ModelViewSet):
     """
     ViewSet for the articles API
@@ -12,12 +26,21 @@ class ArticleViewSet(viewsets.ModelViewSet):
 
     queryset = Article.objects.all().order_by("-creation_date")
     serializer_class = ArticleSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     def get_serializer_class(self):
         if self.action == 'list':
             return ArticleListSerializer
         return ArticleSerializer
+
+    def get_permissions(self):
+        # Anyone can list, create, and retrieve articles, but to update,
+        # we check if the article is locked
+        if self.action in ("list", "create", "retrieve"):
+            permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+        else:
+            permission_classes = (LockedOrAdminPermission,)
+
+        return (permission() for permission in permission_classes)
 
 
 class ArticleRevisionViewSet(viewsets.ModelViewSet):
