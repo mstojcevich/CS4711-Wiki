0d9f8ffe653f2466840bb953af2ecff3267f5331
manunandan95 <44533291+manunandan95@users.noreply.github.com>
2018-12-11 12:50:31 -0500

    Update ArticleListPage.test.jsx

--- wikisite_frontend/src/components/ArticleListPage/ArticleListPage.test.jsx
+++ wikisite_frontend/src/components/ArticleListPage/ArticleListPage.test.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import ArticleListPage from './ArticleListPage';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<ArticleListPage />, div);
+  ReactDOM.render(<Router><ArticleListPage /></Router>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
