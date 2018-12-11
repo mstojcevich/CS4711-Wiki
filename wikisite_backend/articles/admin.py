fdb5cd0bf88be62c3a77fada656ab122cd21606a
Marcus Stojcevich <mstojcevich@gmail.com>
2018-12-11 13:45:54 -0500

    Add ip bans

--- wikisite_backend/articles/admin.py
+++ wikisite_backend/articles/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Article, ArticleRevision
+from .models import Article, ArticleRevision, IPBan
 
 
 admin.site.register(Article)
 admin.site.register(ArticleRevision)
+admin.site.register(IPBan)
--- wikisite_backend/articles/views.py
+++ wikisite_backend/articles/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, permissions
 
-from articles.models import Article, ArticleRevision
+from articles.models import Article, ArticleRevision, IPBan
 from articles.serializers import ArticleSerializer, ArticleListSerializer
 from articles.serializers import ArticleRevisionSerializer
 
@@ -16,6 +16,32 @@ class LockedOrAdminPermission(permissions.BasePermission):
             return False
         else:
             # Article is unlocked or the user is an admin
+            return True
+
+
+def get_client_ip(request):
+    """
+    Determine the IP of the user
+    """
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+class BannedOrAdminPermission(permissions.BasePermission):
+    """
+    Permission that checks to see if the user is unbanned
+    or if the user is an admin
+    """
+
+    def has_object_permission(self, request, view, obj):
+        is_banned = IPBan.objects.filter(ip=get_client_ip(request)).exists()
+        if is_banned and not request.user.is_superuser:
+            return False
+        else:
             return True
 
 
@@ -38,7 +64,7 @@ class ArticleViewSet(viewsets.ModelViewSet):
         if self.action in ("list", "create", "retrieve"):
             permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
         else:
-            permission_classes = (LockedOrAdminPermission,)
+            permission_classes = (LockedOrAdminPermission & BannedOrAdminPermission,)
 
         return (permission() for permission in permission_classes)
 
