807b4edab9bc7465e17db130513c9a6652dfbced
manunandan95 <44533291+manunandan95@users.noreply.github.com>
2018-12-11 21:50:40 -0500

    Update App.jsx

--- wikisite_frontend/src/App.jsx
+++ wikisite_frontend/src/App.jsx
@@ -9,7 +9,7 @@ import LoginPage from './components/LoginPage/LoginPage';
 import HomePage from './components/HomePage/HomePage';
 import ComposePage from './components/ComposePage/ComposePage';
 import ArticleViewPage from './components/ArticleViewPage/ArticleViewPage';
-import ArticleList from './components/ArticleList/ArticleList';
+import ArticleListPage from './components/ArticleListPage/ArticleListPage';
 
 /**
  * App component
@@ -38,7 +38,7 @@ class App extends React.Component {
                     <Route path="/" exact component={() => <HomePage />} />
                     <Route path="/login" exact component={() => <LoginPage onLogin={this.onLogin} />} />
                     <Route path="/compose" exact component={() => <ComposePage />} />
-                    <Route path="/article-list" exact component={() => <ArticleList />} />
+                    <Route path="/article-list" exact component={() => <ArticleListPage />} />
                     <Route
                       path="/articles/:id"
                       exact
