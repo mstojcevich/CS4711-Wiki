29b56a1a7e41678f7fa7c4bb1acec286cb011d15
Anthony Powell <adpowell@mtu.edu>
2018-12-12 18:30:49 -0500

    Clean up lint errors in forked lib

--- wikisite_frontend/src/image-upload-plugin-fork/quill-plugin-image-upload-master/constant.js
+++
@@ -1,6 +0,0 @@
-const constant = {
-  ID_SPLIT_FLAG: '__ID_SPLIT__',
-  IMAGE_UPLOAD_PLACEHOLDER_CLASS_NAME: 'quill-plugin-image-upload-placeholder',
-};
-
-module.exports = constant;
--- wikisite_frontend/src/image-upload-plugin-fork/quill-plugin-image-upload-master/index.js
+++ wikisite_frontend/src/image-upload-plugin-fork/quill-plugin-image-upload-master/index.js
@@ -1,6 +1,4 @@
 require('./ImageUploadPlaceholder.js');
-const imageIdManger = require('./imageIdManger');
-const constant = require('./constant');
 
 class ImageUpload {
   constructor(quill, options) {
@@ -8,7 +6,9 @@ class ImageUpload {
     this.options = options;
     this.range = null;
 
-    if (typeof (this.options.upload) !== 'function') {console.warn('[Missing config] upload function that returns a promise is required');}
+    if (typeof (this.options.upload) !== 'function') {
+      console.warn('[Missing config] upload function that returns a promise is required');
+    }
 
     const toolbar = this.quill.getModule('toolbar');
     toolbar.addHandler('image', this.selectLocalImage.bind(this));
@@ -25,33 +25,22 @@ class ImageUpload {
 
   fileChanged() {
     const file = this.fileHolder.files[0];
-    const imageId = imageIdManger.generate();
 
     const fileReader = new FileReader();
-    fileReader.addEventListener('load', () => {
-      // const base64ImageSrc = fileReader.result;
-      // this.insertBase64Image(base64ImageSrc, imageId);
-    }, false);
     if (file) {
       fileReader.readAsDataURL(file);
     }
 
     this.options.upload(file)
       .then((params) => {
-        this.insertToEditor(params, imageId);
+        this.insertToEditor(params);
       },
       (error) => {
         console.warn(error.message);
       });
   }
 
-  insertBase64Image(url, imageId) {
-    const { range } = this;
-    this.quill.insertEmbed(range.index, 'imageUpload', `${imageId}${constant.ID_SPLIT_FLAG}${url}`);
-  }
-
   insertToEditor(params) {
-    console.log(params)
     this.quill.updateContents(this.quill.getContents().insert({ imageUpload: params.url }, { 'data-meta-url': params.metaUrl }));
   }
 }
